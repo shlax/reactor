e6f647622b00ca6ed6372562c700f3c9aede9393
slax <slax.10101@gmail.com>
2023-02-26 20:51:18 +0100

    time debug

--- src/main/scala/org/reactor/test/HelloBean.java
+++ src/main/scala/org/reactor/test/HelloBean.java
@@ -3,16 +3,20 @@ package org.reactor.test;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
+import java.time.LocalDateTime;
+
 @Named
 @RequestScoped
 public class HelloBean {
 
+    LocalDateTime time;
     public HelloBean(){
-        System.out.println("created:HelloBean");
+        time = LocalDateTime.now();
+        System.out.println("created:HelloBean "+time);
     }
     
     public String hello(){
-        return "hello";
+        return "hello["+time+"]";
     }
 
 }
