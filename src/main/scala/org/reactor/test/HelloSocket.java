153d7a2a0e054fb66f9e824eb29865b1e15da94c
pochodnicky <pochodnicky@gratex.com>
2023-02-27 09:46:15 +0100

    js WebSocket

--- src/main/scala/org/reactor/test/HelloSocket.java
+++ src/main/scala/org/reactor/test/HelloSocket.java
@@ -1,10 +1,16 @@
 package org.reactor.test;
 
+import jakarta.inject.Inject;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 
+import java.io.IOException;
+
 @ServerEndpoint("/time")
 public class HelloSocket {
+
+    @Inject
+    private AppHelloBean appHelloBean;
 
     @OnOpen
     public void onOpen(Session session){
@@ -12,8 +18,9 @@ public class HelloSocket {
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message, Session session) throws IOException {
         System.out.println(session+":"+message);
+        session.getBasicRemote().sendText(appHelloBean.hello());
     }
 
     @OnClose
